a2d39623456cbec77d1413eb8dd6bb0cee1e457e
patelaftab <10patelaftab@gmail.com>
2022-12-23 18:43:46 +0530

    Cheking a Leap Year Program

--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -1,16 +1,18 @@
 ﻿
+using System;
+
 namespace BasicPrograms
 {
     public class Program
     {
         static void Main(string[] args)
         {
-
             //LargestNumber.LargestAmongThree();
             //QuotientAndReminder.FindingValues();
             //EvenOdd.FindingEvenorOdd();
             //SwapNumbers.ChangingNumbers();
-            OvalAndConsonants.CheckingOvalAndConsonants();
+            // OvalAndConsonants.CheckingOvalAndConsonants();
+            LeapYear.CheckingLeapYear();
         }
     }
 }
